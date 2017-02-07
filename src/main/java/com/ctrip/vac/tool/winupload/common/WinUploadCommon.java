5ed31ac576023f877d3b574711cfd3edc8dc5269
vdwq丁卫青 <wqding@ctrip.com>
2017-02-07 09:24:06 +0800

    bug fix: change double from decimal(10,4) to decimal (12,4)
    Signed-off-by: vdwq丁卫青 <wqding@ctrip.com>

--- src/main/java/com/ctrip/vac/tool/winupload/common/WinUploadCommon.java
+++ src/main/java/com/ctrip/vac/tool/winupload/common/WinUploadCommon.java
@@ -67,7 +67,7 @@ public class WinUploadCommon {
 			case LONG:
 				return "bigint";
 			case DOUBLE:
-				return "decimal(10,4)";
+				return "decimal(12,4)";
 			default:
 				if (columnName.endsWith("date")) {
 					return "date";
